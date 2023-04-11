86803cca54733c2572003a708fac979884db7258
bAke19 <blankaake19@gmail.com>
2023-04-11 16:43:24 -0600

    Mi primer commit

--- App.js
+++ App.js
@@ -1,20 +1,95 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
+import {Header} from 'react-native-elements';
+import db from './localdb';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+export default class App extends React.Component{
+
+  constructor(){
+    super();
+    this.state = {
+      texto : '',
+      displayText: '',
+      chunks: ["th", "e"],
+    }
+  }
+  
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <Header
+          backgroundColor='#F3BB25'
+          centerComponent={{
+            text:'Mono Fragmentado',
+            style: {color: "#fff", fontSize: 20}
+          }}
+        />
+        <TextInput
+          style={styles.inputBox}
+          onChangeText={text => {
+            this.setState({texto: text})
+          }}
+          value = {this.state.texto}
+          />
+
+        <TouchableOpacity
+            style={styles.goButton}
+            onPress={() => {
+              var word = this.state.text.toLowerCase().trim();
+              db[word] ? this.setState({ chunks: db[word].chunks }) : console.log("No existe esta palabra en la base de datos")
+            }}>
+
+            <Text style={styles.buttonText}>GO</Text>
+
+        </TouchableOpacity>
+        <View>
+          {this.state.chunks.map(item => {
+            console.log(item)
+            return (
+              <TouchableOpacity
+              style={styles.chunkButton}
+              >
+              <Text style={styles.displayText}>{item}</Text>
+              </TouchableOpacity>
+              );
+          })}
+        </View>
+      </View>
+    );
+  }
+  
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#b8b8b8',
+  },
+  inputBox: {
+    marginTop: 200,
+    width: '80%',
+    alignSelf: 'center',
+    height: 40,
+    textAlign: 'center',
+    borderWidth: 4,
+    outline: 'none',
+  },
+  goButton: {
+    width: '50%',
+    height: 55,
+    alignSelf: 'center',
+    padding: 10,
+    margin: 10,
+  },
+  buttonText: {
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  displayText: {
+    textAlign: 'center',
+    fontSize: 30,
   },
 });
